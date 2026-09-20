85e5d092da4fbc5e503d09a8309d8b4ab2ed25ff
Кирилл Куманцев <kirillkum2001@gmail.com>
2026-09-20 14:06:10 +0300

    Fix: remove path in cfg.

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/High-school-graduation-photo-collection-website/'
+  base: './'
 })
